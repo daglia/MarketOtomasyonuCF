970c39f7501233a0730ba25f409b504771223e57
Furkan Kılıç <furkankilicytu@gmail.com>
2019-01-08 11:57:15 +0300

    Kategoriviewmodel

---
+++ Market.Models/ViewModels/KategoriViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Models.ViewModels
+{
+    public class KategoriViewModel
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAdi { get; set; }
+        public decimal KDV { get; set; }
+       
+        public decimal Kar { get; set; }
+
+        public override string ToString() => $"{KategoriAdi}";
+       
+
+
+    }
+}
